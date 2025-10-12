cb2e25af9f8e4adfc5ef002632a37c12ec756e3b
liton <liton72524nk@gmail.com>
2025-10-13 01:14:18 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,9 @@ from app.services.cf_response_parser import CFResponseParser
 from app.services.svg_factory import SVGFactory
 from app.utils.file_utils import Config
 
-
 def main() -> None:
-    """This method is used for testing purpose."""
     print('generating stats...')
-    CFResponseParser.parse()
+    user = CFResponseParser.parse()  # central User object
 
     generator = SVGFactory.get_svg_generator(Constant.BADGE)
 
@@ -25,7 +23,6 @@ def main() -> None:
     generator.generate(config)
     print('card generation complete.')
     print('output images are saved in the output folder.')
-
 
 if __name__ == '__main__':
     main()

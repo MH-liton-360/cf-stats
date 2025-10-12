44ff4d2d46398740fc58390be0a64173f82dd04a
liton <liton72524nk@gmail.com>
2025-10-13 01:10:28 +0600

    Update cf_response_parser.py

--- app/services/cf_response_parser.py
+++ app/services/cf_response_parser.py
@@ -4,48 +4,70 @@ from app.services.cf_request_handler import CFRequestHandler
 
 class CFResponseParser:
     """Parse responses of Codeforces API."""
-    
-    user = User()  # <-- single shared user instance
 
     @staticmethod
-    def parse():
-        """Parse and make user object."""
+    def parse() -> User:
+        """Parse API responses and return a User object."""
         CFRequestHandler.make_request()
-        CFResponseParser._parse_user_info(CFRequestHandler.user_info)
-        CFResponseParser._parse_user_submission(CFRequestHandler.user_submission)
-        CFResponseParser._parse_rating_changes(CFRequestHandler.rating_changes)
-        return CFResponseParser.user
+        user_info = CFResponseParser._parse_user_info(CFRequestHandler.user_info)
+        submissions = CFResponseParser._parse_user_submission(CFRequestHandler.user_submission)
+        contests = CFResponseParser._parse_rating_changes(CFRequestHandler.rating_changes)
+
+        user = User()
+        # User info
+        user.name = user_info.get('name', '')
+        user.organization = user_info.get('organization', '')
+        user.rating = user_info.get('rating', 0)
+        user.rank = user_info.get('rank', 'newbie')
+        user.max_rating = user_info.get('maxRating', 0)
+        user.max_rank = user_info.get('maxRank', 'newbie')
+        user.contributions = user_info.get('contribution', 0)
+        user.registration_unix_time = user_info.get('registrationTimeSeconds', 0)
+
+        # Submissions info
+        user.submissions = submissions['total']
+        user.accepted = submissions['OK']
+        user.wrong_ans = submissions['WRONG_ANSWER']
+        user.tle = submissions['TIME_LIMIT_EXCEEDED']
+
+        # Contests info
+        user.contests = contests
+
+        return user
 
     @classmethod
     def _parse_user_info(cls, user_info):
-        """Parse and set user's basic profile."""
-        cls.user.name = user_info.get('firstName', '') + ' ' + user_info.get('lastName', '')
-        cls.user.organization = user_info.get('organization', '')
-        cls.user.rating = user_info.get('rating', 0)
-        cls.user.rank = user_info.get('rank', 'newbie')
-        cls.user.max_rating = user_info.get('maxRating', 0)
-        cls.user.max_rank = user_info.get('maxRank', 'newbie')
-        cls.user.contributions = user_info.get('contribution', 0)
-        cls.user.registration_unix_time = user_info.get('registrationTimeSeconds', 0)
+        """Parse basic profile info."""
+        return {
+            'name': user_info.get('firstName', '') + ' ' + user_info.get('lastName', ''),
+            'organization': user_info.get('organization', ''),
+            'rating': user_info.get('rating', 0),
+            'rank': user_info.get('rank', 'newbie'),
+            'maxRating': user_info.get('maxRating', 0),
+            'maxRank': user_info.get('maxRank', 'newbie'),
+            'contribution': user_info.get('contribution', 0),
+            'registrationTimeSeconds': user_info.get('registrationTimeSeconds', 0)
+        }
 
     @classmethod
     def _parse_user_submission(cls, user_submission):
-        """Parse and save submission related details."""
-        cls.user.submissions = len(user_submission)
+        """Parse submission verdicts."""
         freq = defaultdict(int)
         for sb in user_submission:
-            freq[sb.get('verdict', 'UNKNOWN')] += 1
-
-        cls.user.accepted = freq['OK']
-        cls.user.wrong_ans = freq['WRONG_ANSWER']
-        cls.user.tle = freq['TIME_LIMIT_EXCEEDED']
+            freq[sb['verdict']] += 1
+        return {
+            'total': len(user_submission),
+            'OK': freq['OK'],
+            'WRONG_ANSWER': freq['WRONG_ANSWER'],
+            'TIME_LIMIT_EXCEEDED': freq['TIME_LIMIT_EXCEEDED']
+        }
 
     @classmethod
     def _parse_rating_changes(cls, rating_changes):
-        """Sets total number of contests participated by the user."""
-        cls.user.contests = len(rating_changes)
+        """Return number of contests."""
+        return len(rating_changes)
 
 
 if __name__ == '__main__':
     user = CFResponseParser.parse()
-    print(user)
+    print(user.__dict__)
